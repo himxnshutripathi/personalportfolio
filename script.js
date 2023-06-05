19ee668a9f88978237f4ff0d902ab6ba7e9517d9
himxnshutripathi <55108251+himxnshutripathi@users.noreply.github.com>
2023-06-05 22:29:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    });
+});
