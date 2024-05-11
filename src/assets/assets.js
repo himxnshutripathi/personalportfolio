049a4f9f10fab9626047853706a33462ac4c82f0
himxnshutripathi <55108251+himxnshutripathi@users.noreply.github.com>
2024-05-12 01:14:04 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,79 @@
+import header_img from './header_img.png'
+import MainDpLong from './MainDpLong.png'
+import logo from "./mainLogo.png"
+
+import python_ico from "./python_ico.png"
+import javascript_ico from "./javascript_ico.png"
+import reactjs_ico from "./reactjs_ico.png"
+import mysql_ico from "./mysql_ico.png"
+import bootstrap_ico from "./bootstrap_ico.png"
+import git_ico from "./git_ico.png"
+import github_ico from "./github_ico.png"
+import api_ico from "./api_ico.png"
+import tailwindCss_ico from "./tailwindCss_ico.png"
+import linkedin_ico from './linkedin-ico.png'
+import gmail_ico from './gmail-ico.png'
+
+import projectSS_01 from './projectSS_01.png'
+import projectSS_02 from './projectSS_02.png'
+import projectSS_03 from './projectSS_03.png'
+
+export const assExp = {
+    header_img,
+    MainDpLong,
+    logo,
+    projectSS_01,
+    projectSS_02,
+    projectSS_03,
+    gmail_ico,
+    linkedin_ico,
+    github_ico
+}
+
+export const skill_list = [
+    {
+        skill_name: "Python",
+        skill_image: python_ico,
+        skill_link: 'https://docs.python.org/3/'
+    },
+    {
+        skill_name: "JavaScript",
+        skill_image: javascript_ico,
+        skill_link: 'https://devdocs.io/javascript/'
+    },
+    {
+        skill_name: "ReactJS",
+        skill_image: reactjs_ico,
+        skill_link: 'https://legacy.reactjs.org/docs/getting-started.html'
+    },
+    {
+        skill_name: "TailWind CSS",
+        skill_image: tailwindCss_ico,
+        skill_link: 'https://v2.tailwindcss.com/docs'
+    },
+    {
+        skill_name: "My SQL",
+        skill_image: mysql_ico,
+        skill_link: 'https://dev.mysql.com/doc/'
+    },
+    {
+        skill_name: "Boot Strap",
+        skill_image: bootstrap_ico,
+        skill_link: 'https://getbootstrap.com/docs/4.1/getting-started/introduction/'
+    },
+    {
+        skill_name: "Git",
+        skill_image: git_ico,
+        skill_link: 'https://git-scm.com/doc'
+    },
+    {
+        skill_name: "Github",
+        skill_image: github_ico,
+        skill_link: 'https://docs.github.com/en'
+    },
+    {
+        skill_name: "Rest API",
+        skill_image: api_ico,
+        skill_link: 'https://docs.github.com/en/rest?apiVersion=2022-11-28'
+    }
+]
