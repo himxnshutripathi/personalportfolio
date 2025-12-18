32699d74ec14c22cc6928f168b5bf8b213829700
himxnshutripathi <tripathi.himanshu9329@gmail.com>
2025-12-18 14:18:33 +0530

    Update Aboutme.jsx

--- src/Components/Aboutme/Aboutme.jsx
+++ src/Components/Aboutme/Aboutme.jsx
@@ -12,11 +12,12 @@ const Aboutme = () => {
             <h2>About Me</h2>
             <hr />
             <h4>Developer <span>& Web Designer</span></h4>
-            <p>Hi, I'm Himanshu Tripathi a Frontend Developer, Programmer and Web designer. I've completed my bachelor's degree in Computer Science and Engineering from Lovely Professional University.</p>
+            <p>Hi, I'm Himanshu Tripathi a Full Stack Developer, Programmer and Web designer. I've completed my bachelor's degree in Computer Science and Engineering from Lovely Professional University.</p>
             <button><a href="mailto:tripathi.himanshu9329@gmail.com">Let's get Connected</a></button>
         </div>
     </div>
   )
 }
+
 
 export default Aboutme
