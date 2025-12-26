10ddbd37f2c85e17872d6abbd4e2362b0b83358d
himxnshutripathi <tripathi.himanshu9329@gmail.com>
2025-12-26 14:10:37 +0530

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -20,12 +20,13 @@ const Header = () => {
               </span><Cursor cursorStyle="_"/></p>
             <div className="buttons">
                 <button><a href="mailto:tripathi.himanshu9329@gmail.com">Hire Me</a></button>
-                <button><a href="https://drive.google.com/file/d/1InTVitk1az5fvaNFxRY4fZA_1lju7ckh/view?usp=drive_link" target='_blank'>Get Resume</a></button>
+                <button><a href="https://drive.google.com/file/d/19_eSnGSIfGWsPoZMaDxlXdmV5OLqfNxF/view?usp=sharing" target='_blank'>Get Resume</a></button>
                 <button><a href="https://github.com/himxnshutripathi" target='_blank'>Github Profile</a></button>
             </div>
         </div>
     </div>
   )
 }
+
 
 export default Header
